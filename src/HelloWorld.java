9ef08473a0c68088733cfbbad3575088340c7c89
student <student@158.197.35.85>
2018-09-19 09:22:37 +0200

    Super zmena

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,17 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		for (int i = 0; i < 10; i++) {
-			System.out.println("GITTTTT");
-			System.out.println("OU YEAH");
-		}
-		// cau, to som ja, Lucka
-		// cau, aj Jakub je tu
-		System.out.println("som pridal");
-		
-		System.out.println("Merge with my version");
+		//Vymazane
 	}
 
 }
