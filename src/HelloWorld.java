9ce103f196887f401a6bd4dc80d315a8443110ae
student <student@158.197.35.87>
2018-09-19 07:32:49 +0200

    10x GITTT
    pridany for cyklus
    toto so tiez kodil v noci

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("GITTTTT");
+		}
 
 	}
 
